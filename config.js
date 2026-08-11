fdeb24fe9de53ecee29cef68687d691f71c46cc4
ytadistrictkashmore <zainmeeeer@gmail.com>
2026-08-12 02:03:18 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-
 window.YTA_CONFIG = {
   supabaseUrl: "https://iqwauodmugcbxlmdvqas.supabase.co",
-  supabaseAnonKey: "sb_publishable_rx8GDyrYmlMKlB4iB8Teug_FiB7iGNM"
+  supabaseAnonKey: "YOUR_PUBLISHABLE_KEY"
 };
