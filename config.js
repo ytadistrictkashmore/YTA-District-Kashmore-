6ad0547b89f6e130e25acd103b46b31076fc4fde
ytadistrictkashmore <zainmeeeer@gmail.com>
2026-08-12 08:44:38 +0500

    Remove Supabase configuration from config.js
    Removed sensitive Supabase configuration from config.js.

--- config.js
+++ config.js
@@ -1,4 +1,1 @@
-window.YTA_CONFIG = {
-  supabaseUrl: "https://iqwauodmugcbxlmdvqas.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlxd2F1b2RtdWdjYnhsbWR2cWFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2NTQwMDAsImV4cCI6MjA3MjkxMDAwMH0.T3vF-qP47v3Kx-6yR-3xV1d9p-5Xv_lW4t5p_Gz_Q_s"
-};
+
